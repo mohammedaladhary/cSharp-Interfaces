98f1a0f8ae3316eaa72266e1990e989dab2b9d67
mohammedaladhary <caladhary@gmail.com>
2023-06-21 13:17:14 +0400

    interfaces shape exercise

---
+++ IShape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces
+{
+    internal interface IShape
+    {
+        public interface IShape
+        {
+            double calculateArea();
+            double calculatePerimeter();
+        }
+    }
+    public class Rectangle : IShape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public double getArea()
+        {
+            return length*width;
+        }
+        public double getPerimeter()
+        {
+            return 2*(length + width);
+        }
+    }
+    public class Circle : IShape
+    {
+        private double radius;
+        
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double getArea() {
+        return Math.PI * radius * radius;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine("Circle - Area: " + circle.getArea());
+            Console.WriteLine("Rectangle - Area: " + rectangle.getArea());
+            Console.WriteLine("Circle - Perimeter: " + circle.getPerimeter());
+            Console.WriteLine("Rectangle - Perimeter: " + rectangle.getPerimeter());
+
+        }
+    }
+}
