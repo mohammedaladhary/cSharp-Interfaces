192825abba0ca14f0580a769a66faf7317ce5944
mohammedaladhary <caladhary@gmail.com>
2023-06-22 13:30:02 +0400

    for each loop

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces
+{
+    internal class Person
+    {
+        String name;
+        readonly String username;
+        int age;
+        const int abc = 5;
+
+        public Person(String name, String username, int age)
+        {
+            this.name = name;
+            this.username = username;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return this.name + " " + this.username + " " + this.age;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,6 +19,24 @@ namespace interfaces
             Square square1 = new Square(2);
 
             Console.WriteLine(square.CompareTo(square1));
+
+            int[] i = {1,2,3,4,5,6,7};
+            for(int a = 0; a < i.Length; a++)
+            {
+                Console.WriteLine(i[a]);
+            }
+            Console.WriteLine("Printing using for each");
+            Person[] people = new Person[3];
+            people[0] = new Person("Mohammed", "16-0450", 24);
+            people[1] = new Person("Ahmed", "25-4324", 54);
+            people[2] = new Person("Hamed", "16-4532", 10);
+
+            foreach(Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+
         }
     }
 }
